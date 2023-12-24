24cff6d382b37461a63e466d6351b9468c48ad87
tphat <138270919+huynhletanphat@users.noreply.github.com>
2023-12-24 16:09:31 +0700

    Create pexels.js
    v1.0.0

---
+++ pexels.js
@@ -0,0 +1,53 @@
+      const config = {
+        name: "pexels",
+        aliases: ["pexels", "searchimg", "image", "img"],
+        description: "Searching Images Pexels",
+        usage: "pexels <text>",
+        versinos: "1.0.0",
+        credits: "github.com/huynhletanphat",
+        cooldown: 5
+      };
+
+const langData = {
+  "vi_VN": {
+    "missingInput": "Bạn chưa nhập từ khóa tìm kiếm",
+    "error": "Đã có lỗi xảy ra"
+  },
+  "en_US": {
+    "missingInput": "You haven't entered a search keyword",
+    "error": "An error has occurred"
+  },
+  "ar_SY": {
+    "missingInput": "لم تدخل كلمة البحث",
+    "error": "حدث خطأ"
+  }
+};
+
+async function onCall({ message, args, getLang }) {
+  try {
+    const input = args.join(" ");
+    if (!input) return message.reply(getLang("missingInput"));
+
+    const res = await global.GET(`https://project-api.phattan14.repl.co/media/pexels?img=${encodeURIComponent(input)}`, {
+      timeout: 120000
+    });
+    const data = res.data;
+
+    if (!data.img) return message.reply(getLang("error"));
+
+    const imgStream = await global.getStream(data.img);
+    await message.reply({
+      attachment: [imgStream]
+    });
+    
+  } catch (e) {
+    console.error(e);
+    message.reply(getLang("error"));
+  }
+}
+
+export default {
+  config,
+  langData,
+  onCall
+};
