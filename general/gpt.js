95bbd5c19829b6acb9bc90c8c9b112082ee5c7ad
Huynh Le Tan Phat <138270919+huynhletanphat@users.noreply.github.com>
2024-04-22 20:03:30 +0700

    Create gpt.js

---
+++ general/gpt.js
@@ -0,0 +1,45 @@
+const config = {
+  name: "gpt",
+  credits: "Tanphat",
+  cooldown: 5,
+};
+
+const langData = {
+  vi_VN: {
+    missingInput: "Vui lòng nhập text",
+    notFound: "nể luôn",
+    results:
+      "{gpt4}",
+    error: "Đã có lỗi xảy ra!",
+  },
+};
+
+async function onCall({ message, args, getLang }) {
+  try {
+    const input = args.join("+").toLowerCase();
+    if (!input) return message.reply(getLang("missingInput"));
+    const encodedInput = encodeURIComponent(input);
+    const res = await global.GET(
+      `https://deku-rest-api.replit.app/gpt4?prompt=${encodedInput}&uid=tphatthankyou`,
+    );
+
+    const GPTdata = res.data;
+
+    if (!GPTdata) return message.reply(getLang("notFound"));
+
+    return message.reply(
+      getLang("results", {
+        gpt4: GPTdata,
+      }),
+    );
+  } catch (e) {
+    console.error(e);
+    message.reply(getLang("error"));
+  }
+}
+
+export default {
+  config,
+  langData,
+  onCall,
+};
